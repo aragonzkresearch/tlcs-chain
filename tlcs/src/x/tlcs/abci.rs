6e31ae8a11710d36ddb2b95c5255f394b0422249
Kevin Jones <kevin.jonesat@gmail.com>
2023-06-29 13:46:44 +0100

    Write secret key to tlcs store in begin blocker

--- tlcs/src/x/tlcs/abci.rs
+++ tlcs/src/x/tlcs/abci.rs
@@ -1,4 +1,4 @@
-use std::ops::RangeFrom;
+use std::{collections::HashMap, ops::RangeFrom};
 
 use crate::{
     error::AppError,
@@ -9,15 +9,9 @@ use bytes::Bytes;
 use database::{PrefixDB, DB};
 use prost::Message;
 
-use proto_messages::azkr::tlcs::v1beta1::{
-    RawMsgKeyPair,
-    RawMsgLoeData,
-};
+use proto_messages::azkr::tlcs::v1beta1::{RawMsgKeyPair, RawMsgLoeData};
 
-use super::crypto::{
-    aggregate_participant_data,
-    make_secret_key,
-};
+use super::crypto::{aggregate_participant_data, make_secret_key};
 
 const LOE_GENESIS_TIME: u32 = 1677685200;
 const LOE_PERIOD: u32 = 3;
@@ -31,7 +25,6 @@ const LOE_DATA_KEY: [u8; 1] = [3];
 // TODO eliminate in the future to support multiple schemes
 const TMP_SCHEME_ID: [u8; 1] = [1];
 
-
 pub fn begin_blocker<T: DB>(ctx: &mut TxContext<T>) {
     let last_processed_round = get_last_processed_round(ctx);
     let block_time = ctx.get_header().time.unix_timestamp();
@@ -39,11 +32,11 @@ pub fn begin_blocker<T: DB>(ctx: &mut TxContext<T>) {
 
     // TODO Later this should be added
     //for scheme in LIST_OF_SCHEMES {
-    
+
     for round in last_processed_round..process_up_to {
         // TODO Add the scheme in here
         let round_all_participant_data = get_this_round_all_participant_data(ctx, round);
-      
+
         let round_data = round_all_participant_data.fold((0, vec![]), |mut acc, e| {
             acc.1.extend(e.1);
             acc.0 += acc.0;
@@ -66,13 +59,11 @@ pub fn begin_blocker<T: DB>(ctx: &mut TxContext<T>) {
     let _ = set_last_processed_round(ctx, process_up_to);
 }
 
-fn make_keys<'a, T: DB>(
-    ctx: &'a TxContext<T>,
-    time: i64, 
-) {
+fn make_keys<'a, T: DB>(ctx: &'a mut TxContext<T>, time: i64) {
     let tlcs_store = ctx.get_kv_store(Store::Tlcs);
     let store_key = KEYPAIR_DATA_KEY.to_vec();
     let keypairs = tlcs_store.range(store_key..);
+    let mut tmp_store: HashMap<Vec<u8>, Vec<u8>> = HashMap::new();
 
     let latest_round = latest_round_up_to(time);
 
@@ -94,11 +85,11 @@ fn make_keys<'a, T: DB>(
             });
 
             let secret_key = make_secret_key(
-                    round_data.1,
-                    round_data.0,
-                    key_round,
-                    loe_round_data.signature,
-                    the_keys.public_key.clone(),
+                round_data.1,
+                round_data.0,
+                key_round,
+                loe_round_data.signature,
+                the_keys.public_key.clone(),
             );
 
             the_keys.private_key = secret_key;
@@ -107,15 +98,18 @@ fn make_keys<'a, T: DB>(
             let mut this_store_key = KEYPAIR_DATA_KEY.to_vec();
             this_store_key.append(&mut key_round.to_le_bytes().to_vec());
             this_store_key.append(&mut TMP_SCHEME_ID.to_vec());
-            
-            //tlcs_store.set(this_store_key.into(), the_keys.encode_to_vec());
+
+            tmp_store.insert(this_store_key.into(), the_keys.encode_to_vec());
         }
+    }
+
+    let tlcs_store = ctx.get_mutable_kv_store(Store::Tlcs);
+    for (k, v) in tmp_store {
+        tlcs_store.set(k, v)
     }
 }
 
-fn keypair_key_to_round(
-    key: Vec<u8>,
-) -> u32 {
+fn keypair_key_to_round(key: Vec<u8>) -> u32 {
     // Get rid of first part. That is the KEYPAIR_DATA_KEY
     let (_, rest) = key.split_at(1);
     let short_key = rest.to_vec();
@@ -138,7 +132,7 @@ fn get_last_processed_round<T: DB>(ctx: &mut TxContext<T>) -> u32 {
 
 fn set_last_processed_round<T: DB>(
     ctx: &mut TxContext<T>,
-    last_round: u32
+    last_round: u32,
 ) -> Result<(), AppError> {
     let tlcs_store = ctx.get_mutable_kv_store(Store::Tlcs);
     let prefix = LAST_PROCESSED_ROUND_KEY.to_vec();
@@ -149,7 +143,6 @@ fn set_last_processed_round<T: DB>(
 
 fn process_up_to(time: i64) -> u32 {
     latest_round_up_to(time) + LOE_PERIOD
-
 }
 
 /// Returns the latest loe round expected before the provided unix time
@@ -169,10 +162,7 @@ fn get_this_round_all_participant_data<'a, T: DB>(
     tlcs_store.range(prefix..)
 }
 
-fn get_this_round_all_loe_data<'a, T: DB>(
-    ctx: &'a TxContext<T>,
-    round: u32,
-) -> RawMsgLoeData {
+fn get_this_round_all_loe_data<'a, T: DB>(ctx: &'a TxContext<T>, round: u32) -> RawMsgLoeData {
     let tlcs_store = ctx.get_kv_store(Store::Tlcs);
 
     let mut prefix = LOE_DATA_KEY.to_vec();
@@ -193,8 +183,7 @@ fn get_this_round_all_loe_data<'a, T: DB>(
     //let loe_data: RawMsgLoeData = RawMsgLoeData::decode::<Bytes>(store_data.into())
     //    .expect("invalid data in database - possible database corruption");
 
-    return loe_data
-
+    return loe_data;
 }
 
 fn get_public_keys_store<'a, T: DB>(
