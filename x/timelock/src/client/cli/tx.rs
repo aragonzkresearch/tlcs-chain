da3762cddcb5b3d9fff8860464ca5056716b548f
craigsailor <craigsailor@gmail.com>
2023-10-06 11:33:33 +0200

    changed loe params and moved them all into lib.rs

--- x/timelock/src/client/cli/tx.rs
+++ x/timelock/src/client/cli/tx.rs
@@ -11,11 +11,9 @@ use tokio::runtime::Runtime;
 use crate::Message as TimelockMessage;
 use tlcs_rust::chain_functions::make_keyshare;
 
-const LOE_PUBLIC_KEY: &str = "a0b862a7527fee3a731bcb59280ab6abd62d5c0b6ea03dc4ddf6612fdfc9d01f01c31542541771903475eb1ec6615f8d0df0b8b6dce385811d6dcf8cbefb8759e5e616a3dfd054c928940766d9a5b9db91e3b697e5d70a975181e007f87fca5e";
-const SECURITY_PARAM: usize = 10;
-//const SCHEME: &str = "BJJ";
-const LOE_URL: &str =
-    "https://api.drand.sh/dbd506d6ef76e5f386f41c651dcb808c5bcbd75471cc4eafa3f4df7ad4e4c493/";
+use crate::LOE_PUBLIC_KEY;
+use crate::LOE_URL;
+use crate::SECURITY_PARAM;
 
 #[derive(Args, Debug)]
 pub struct Cli {
