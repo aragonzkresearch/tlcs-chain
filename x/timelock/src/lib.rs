e676d58da26df549a5b4c07d32866a2ca0cd9ba9
craigsailor <craigsailor@gmail.com>
2023-10-10 15:13:11 +0900

    update LOE public key for quicknet

--- x/timelock/src/lib.rs
+++ x/timelock/src/lib.rs
@@ -16,7 +16,7 @@ pub use message::*;
 // This is the "quicknet"
 pub const LOE_URL: &str =
     "https://api.drand.sh/52db9ba70e0cc0f6eaf7803dd07447a1f5477735fd3f661792ba94600c84e971/";
-const LOE_PUBLIC_KEY: &str = "a0b862a7527fee3a731bcb59280ab6abd62d5c0b6ea03dc4ddf6612fdfc9d01f01c31542541771903475eb1ec6615f8d0df0b8b6dce385811d6dcf8cbefb8759e5e616a3dfd054c928940766d9a5b9db91e3b697e5d70a975181e007f87fca5e";
+const LOE_PUBLIC_KEY: &str = "83cf0f2896adee7eb8b5f01fcad3912212c437e0073e911fb90022d3e760183c8c4b450b6a0a6c3ac6a5776a2d1064510d1fec758c921cc22b0e17e63aaf4bcb5ed66304de9cf809bd274ca73bab4af5a6e9c76a4bc09e76eae8991ef5ece45a";
 pub const LOE_GENESIS_TIME: u32 = 1692803367;
 pub const LOE_PERIOD: u32 = 3;
 const SECURITY_PARAM: usize = 10;
